d358e74488ffc75aa990d97e8062d624a3419677
Faizan Ali <fa1694701@gmail.com>
2023-09-18 03:09:45 -0700

    new changing

---
+++ components/Header/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div className="bg-cyan-600 h-16">
+            <h1 className="text-center text-2xl font-serif pt-3">Todo App</h1>
+
+        </div>
+    )
+}
+export default Header;
